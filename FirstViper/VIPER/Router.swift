991889b9f7349b45739cae53a199226f90e34dd0
hafied17 <hafiedmadrid6@gmail.com>
2024-02-13 13:11:19 +0700

    first

--- FirstViper/VIPER/Router.swift
+++ FirstViper/VIPER/Router.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import UIKit
+
+// object
+// entry point
+
+typealias EntryPoint = AnyView & UIViewController
+
+protocol AnyRouter {
+    var entry: EntryPoint? { get set }
+    
+    static func start() -> AnyRouter
+}
+
+class UserRouter: AnyRouter {
+    var entry: EntryPoint?
+    
+    
+    static func start() -> AnyRouter {
+        let router = UserRouter()
+        
+        // assign VIP
+        var view: AnyView = UserViewController()
+        var presenter: AnyPresenter = UserPresenter()
+        var interactor: AnyInteractor = UserInteractor()
+        
+        view.presenter = presenter
+        
+        interactor.presenter = presenter
+        interactor.getUsers()
+        
+        presenter.router = router
+        presenter.view = view
+        presenter.interactor = interactor
+        
+        router.entry = view as? EntryPoint
+        
+        return router
+        
+    }
+    
+}
--- FirstViper/VIPER/View.swift
+++ FirstViper/VIPER/View.swift
@@ -5,4 +5,80 @@
 //  Created by Investo Medika Asia on 13/02/24.
 //
 
-import Foundation
+import UIKit
+
+// ViewController
+// protocol
+// reference presenter
+
+protocol AnyView {
+    var presenter: AnyPresenter? { get set }
+    
+    func update(with users: [User])
+    func update(with error: String)
+}
+
+class UserViewController: UIViewController, AnyView, UITableViewDelegate, UITableViewDataSource {
+    
+    var presenter: AnyPresenter?
+    var users = [User]()
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.isHidden = true
+        return table
+    }()
+    
+    private let labelText: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.isHidden = true
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(labelText)
+        view.addSubview(tableView)
+        view.backgroundColor = .systemPink
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+        labelText.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+        labelText.center = view.center
+    }
+    
+    func update(with users: [User]) {
+        DispatchQueue.main.async {
+            self.users = users
+            self.tableView.reloadData()
+            self.tableView.isHidden = false
+        }
+    }
+    
+    func update(with error: String) {
+        DispatchQueue.main.async {
+            self.users = []
+            self.tableView.isHidden = true
+            self.labelText.isHidden = false
+            self.labelText.text = error
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = users[indexPath.row].names
+        return cell
+    }
+    
+    
+}
